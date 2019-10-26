30f3344056c28b141b0e6692df79f423a2da4d9f
hemanth06912 <hemanth069122019@gmail.com>
2019-10-26 20:34:58 +0200

    js3Week1

--- javascript/javascript3/week1/gifhy/gifhy.js
+++ javascript/javascript3/week1/gifhy/gifhy.js
@@ -2,8 +2,10 @@
 // https://api.giphy.com/v1/stickers/search?api_key=vwMiYSudtzSswVZqhOPRMxtUs93gvYXk&q=&limit=25&offset=0&rating=G&lang=en
 
 document.getElementById('search').addEventListener('click',getJson);
-function getJson(){   
-     fetch('https://api.giphy.com/v1/gifs/search?api_key=vwMiYSudtzSswVZqhOPRMxtUs93gvYXk&q=cats&limit=25&offset=0&rating=G&lang=en')
+function getJson(){ 
+    const input=document.querySelector('#giphy').value 
+    const number = document.querySelector('#number').value 
+     fetch('https://api.giphy.com/v1/gifs/search?api_key=vwMiYSudtzSswVZqhOPRMxtUs93gvYXk&q='+ input +'&limit='+ number +'&offset=0&rating=G&lang=en')
     .then( res => res.json())
     .then(data => {
         searchGif(data)
@@ -14,9 +16,13 @@ function getJson(){
 
 function searchGif(data){
 
-    for(i=0;i<data.data.length;i++){
+    for(let i=0; i<data.data.length; i++){
+        const gifsy = document.querySelector('#content');
+        let h3 = document.createElement("h3");
+        h3.textContent = data.data[i].title;
+        gifsy.appendChild(h3);
         const image = document.createElement('img');
-        output.appendChild(image);
-        output.src=data.data[i].images.original.url;
+        gifsy.appendChild(image);
+        image.src=data.data[i].images.fixed_height.url;
     }
 }
