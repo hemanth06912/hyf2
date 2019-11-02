01e8ebdbb7bd7bc4963f8a49704ad4e0e871826a
hemanth06912 <hemanth069122019@gmail.com>
2019-11-02 23:42:49 +0100

    js3Week2

--- javascript/javascript3/week2/visualPromise/main.js
+++ javascript/javascript3/week2/visualPromise/main.js
@@ -1,17 +1,35 @@
+    const redBox = document.querySelector('ul.marks li:nth-child(1)');
+    const blueBox = document.querySelector('ul.marks li:nth-child(2)');
+    const greenBox = document.querySelector('ul.marks li:nth-child(3)');
+    const boxes = [redBox, blueBox, greenBox];
+
+
+
+
 function translateOneByOne(){
-    moveElement(document.querySelector('li'), {x: 20, y: 300})
-    .then(() => {
-        console.log('red Element has been moved');
+    moveElement(redBox,{x: 20, y: 300 })
+    .then(()=> {
+        console.log('red Element has been moved')
     })
-    moveElement(document.querySelector('li'), {x: 400, y: 300})
+    .then(()=>moveElement(blueBox,{x: 400, y: 300 }))
     .then(() => {
-        console.log('red Element has been moved');
+        console.log('blue Element has been moved')
     })
-    moveElement(document.querySelector('li'), {x: 400, y: 20})
+    .then(()=>moveElement(greenBox,{x: 400, y: 20 }))
     .then(() => {
-        console.log('red Element has been moved');
+        console.log('green Element has been moved')
     })
     
 }
 
 translateOneByOne();
+const promise1= moveElement(redBox,{x: 20, y: 300 });
+const promise2= moveElement(blueBox,{x: 400, y: 300 });
+const promise3= moveElement(blueBox,{x: 400, y: 20 });
+
+function translateAllAtOnce(){
+    Promise.all([promise1,promise2,promise3])
+    .then(()=>console.log('All elements are moved'))
+}
+
+translateAllAtOnce();
